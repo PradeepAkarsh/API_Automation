7c77c7fa2035001f4cfaef171d6170c65728ca03
PradeepAkarsh <38950239+PradeepAkarsh@users.noreply.github.com>
2024-04-20 22:50:41 +0530

    Delete src/test/java/pojo directory
    no needed anymore

--- src/test/java/pojo/API_Constants.java
+++
@@ -1,7 +0,0 @@
-package pojo;
-
-public class API_Constants {
-    final static String baseURL = "https://thinking-tester-contact-list.herokuapp.com";
-    final static String endPoint = "/users/login";
-    final static String addContact ="/contacts";
-}
